15f96b8afcafcc6c9af72a2440c35a22e5c3c8d4
RexLiu <rexliu@thinkgeo.com>
2016-11-25 17:03:13 +0800

    Upload code.

---
+++ DistanceQueryOnWrapDatelineMode/TestWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using ThinkGeo.MapSuite;
+using ThinkGeo.MapSuite.Drawing;
+using ThinkGeo.MapSuite.Layers;
+using ThinkGeo.MapSuite.Shapes;
+using ThinkGeo.MapSuite.Styles;
+using ThinkGeo.MapSuite.Wpf;
+
+namespace DistanceQueryOnWrapDatelineMode
+{
+    /// <summary>
+    /// Interaction logic for TestWindow.xaml
+    /// </summary>
+    public partial class TestWindow : Window
+    {
+        public TestWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Sets the correct map unit 
+            wpfMap1.MapUnit = GeographyUnit.DecimalDegree;
+            //Sets the map extent in real coordinates.
+            wpfMap1.CurrentExtent = new RectangleShape(-326,82,-58,-91);
+            wpfMap1.Background = new SolidColorBrush(Color.FromRgb(148, 196, 243));
+
+            WorldMapKitWmsWpfOverlay worldMapKitWmsWpfOverlay = new WorldMapKitWmsWpfOverlay();
+            //Sets the WrapDatelineMode to WrapDateline for the overlay.
+            worldMapKitWmsWpfOverlay.WrappingMode = WrappingMode.WrapDateline;
+            wpfMap1.Overlays.Add("WMK",worldMapKitWmsWpfOverlay);
+
+            //InMemoryFeatureLayer to put the feature as the result of the spatial query.
+            InMemoryFeatureLayer inMemoryFeatureLayer = new InMemoryFeatureLayer();
+            inMemoryFeatureLayer.ZoomLevelSet.ZoomLevel01.DefaultAreaStyle = AreaStyles.CreateSimpleAreaStyle
+                (GeoColor.FromArgb(150, GeoColor.StandardColors.Yellow), GeoColor.StandardColors.DarkGray, 2);
+            inMemoryFeatureLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+
+            LayerOverlay layerOverlay = new LayerOverlay();
+            //Sets the WrapDatelineMode to WrapDateline for the overlay with the InMemoryFeatureLayer.
+            layerOverlay.WrappingMode = WrappingMode.WrapDateline;
+            layerOverlay.Layers.Add("SelectLayer", inMemoryFeatureLayer);
+            wpfMap1.Overlays.Add("DynamicOverlay", layerOverlay);
+
+            wpfMap1.Refresh();
+        }
+
+        private void wpfMap1_MapClick(object sender, MapClickWpfMapEventArgs e)
+        {
+            try
+            {
+                //Uses the WrapDatelineProjection to create the target pointshape for GetFeatureNearestTo spatial query function.
+                WrapDatelineProjection wrapDatelineProjection = new WrapDatelineProjection();
+                //Sets the HalfExtentWidth of the wrapdateline overlay we want to apply the projection on.
+                //Here it is 180 because, the full extent width is 360.
+                wrapDatelineProjection.HalfExtentWidth = wpfMap1.Overlays["WMK"].GetBoundingBox().Width / 2;//180;
+
+                //Gets the valid world coordinate regardless to where the user click on the map
+                wrapDatelineProjection.Open();
+                Vertex projVertex = wrapDatelineProjection.ConvertToExternalProjection(e.WorldX, e.WorldY);
+                wrapDatelineProjection.Close();
+
+                //Here we just use the feature source of the shapefile because we don't display it, we just use it for doing the spatial query.
+                ShapeFileFeatureSource shapeFileFeatureSource = new ShapeFileFeatureSource(@"../../data/countries02.shp");
+                shapeFileFeatureSource.Open();
+                //Uses the projected X and Y values for the Spatial Query.
+                Collection<Feature> features = shapeFileFeatureSource.GetFeaturesNearestTo(new PointShape(projVertex), wpfMap1.MapUnit, 1, ReturningColumnsType.NoColumns);
+                shapeFileFeatureSource.Close();
+
+                LayerOverlay dynamicOverlay = (LayerOverlay)wpfMap1.Overlays["DynamicOverlay"];
+                InMemoryFeatureLayer inMemoryFeatureLayer = (InMemoryFeatureLayer)dynamicOverlay.Layers["SelectLayer"];
+
+                //Clears the InMemoryFeatureLayer and add the feature as the result of the spatial query.
+                inMemoryFeatureLayer.Open();
+                inMemoryFeatureLayer.InternalFeatures.Clear();
+                inMemoryFeatureLayer.InternalFeatures.Add(features[0]);
+                inMemoryFeatureLayer.Close();
+
+                //Refreshes only the overlay with the updated InMemoryFeatureLayer.
+                wpfMap1.Refresh(dynamicOverlay);
+            }
+            catch { }
+            finally { }
+        }
+
+       
+        private void wpfMap1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Gets the PointShape in world coordinates from screen coordinates.
+            Point point = e.MouseDevice.GetPosition(null);
+            
+            ScreenPointF screenPointF = new ScreenPointF((float)point.X, (float)point.Y);
+            PointShape pointShape = ExtentHelper.ToWorldCoordinate(wpfMap1.CurrentExtent, screenPointF, (float)wpfMap1.Width, (float)wpfMap1.Height);
+
+            //Uses the WrapDatelineProjection to get the proper decimal degree value on the virtual maps to the right and left of the central map.
+            WrapDatelineProjection wrapDatelineProjection = new WrapDatelineProjection();
+            //Sets the HalfExtentWidth of the wrapdateline overlay we want to apply the projection on.
+            //Here it is 180 because, the full extent width is 360.
+            wrapDatelineProjection.HalfExtentWidth = wpfMap1.Overlays["WMK"].GetBoundingBox().Width / 2;//180;
+
+            wrapDatelineProjection.Open();
+            Vertex projVertex = wrapDatelineProjection.ConvertToExternalProjection(pointShape.X, pointShape.Y);
+            wrapDatelineProjection.Close();
+
+            try
+            {
+                //Shows the real coordinates.
+                textBox1.Text = "real X: " + string.Format("{0:0.00}", System.Math.Round(pointShape.X, 2)) +
+                             "  real Y: " + string.Format("{0:0.00}", System.Math.Round(pointShape.Y, 2));
+                //Shows the decimal degrees after the WrapDatelineProjection
+                textBox2.Text = "Decimal Degree X: " + string.Format("{0:0.00}", System.Math.Round(projVertex.X, 2)) +
+                             " Decimal Degree Y: " + string.Format("{0:0.00}", System.Math.Round(projVertex.Y, 2));
+                //Shows the Decimal Minutes Seconds format after the WrapDatelineProjection
+                textBox3.Text = "Long.: " + DecimalDegreesHelper.GetDegreesMinutesSecondsStringFromDecimalDegree(projVertex.X) +
+                              "  Lat.: " + DecimalDegreesHelper.GetDegreesMinutesSecondsStringFromDecimalDegree(projVertex.Y);
+            }
+            catch { }
+            finally { }
+
+            }
+      }
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# DistanceQueryOnWrapDatelineModeSample-ForWpf
+
+### Description
+
+                In this Wpf sample is on the same theme as the previous sample WrapDatelineMode. Here we show how to take advantage of the projection class WrapDatelineProjection for doing spatial query when on a “virtual map”. From this spatial query example, the developer should understand how the same principle using the same projection class can be applied for other operations such as Spatial Queries, Identify etc. In order to run this sample, you will need the latest Dev Daily Build for Wpf with references to MapSuiteCore.dll and WpfDesktopEdition.dll.
+              
+
+![Screenshot](https://github.com/ThinkGeo/DistanceQueryOnWrapDatelineModeSample-ForWpf/blob/master/ScreenShot.png)
+
+### Requirements
+This sample makes use of the following NuGet Packages
+
+[MapSuite 10.0.0.0](http:mapsuite.nuget)
+
+### About the Code
+
+Working...
+
+### Getting Help
+
+[Map Suite Wpf Wiki Resources](http://wiki.thinkgeo.com/wiki/map_suite_wpf_desktop_edition)
+
+[Map Suite Wpf Product Description](http://thinkgeo.com/map-suite-developer-gis/wpf-edition/)
+
+[ThinkGeo Community Site](http://community.thinkgeo.com/)
+
+[ThinkGeo Web Site](http://www.thinkgeo.com)
+
+### Key APIs
+This example makes use of the following APIs:
+
+Working...
+
+### About Map Suite
+Map Suite is a set of powerful development components and services for the .Net Framework.
+
+### About ThinkGeo
+ThinkGeo is a GIS (Geographic Information Systems) company founded in 2004 and located in Frisco, TX. Our clients are in more than 40 industries including agriculture, energy, transportation, government, engineering, software development, and defense.
